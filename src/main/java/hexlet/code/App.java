01666ed7a41d4bd5cbf172720a9ddd395641592c
Evgeniy Pautov <pautov.e@yandex.ru>
2022-05-05 12:30:48 +0300

    Add Picocli dependency

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,42 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+
+    @Option(names = {"-f", "--format"},description = "output format [default: stylish]")
+    private String format;
+
+    @Parameters(description = "path to first file")
+    private String filepath1;
+
+    @Parameters(description = "path to second file")
+    private String filepath2;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.parseArgs(args);
+
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+        } else if (commandLine.isVersionHelpRequested()) {
+            commandLine.printVersionHelp(System.out);
+        }
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return null;
     }
 }
