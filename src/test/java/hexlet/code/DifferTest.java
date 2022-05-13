159f5d75b314c9d565a2a53d9159f75d148d8ef9
Evgeniy Pautov <pautov.e@yandex.ru>
2022-05-13 14:05:26 +0300

    Add more tests

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -10,25 +10,58 @@ import java.nio.file.Path;
 class DifferTest {
 
     private final String pathToResources = "src/test/resources/";
+    private final String fileJsonPath1 = pathToResources + "plain1.json";
+    private final String fileJsonPath2 = pathToResources + "plain2.json";
+    private final String fileYamlPath1 = pathToResources + "plain1.yaml";
+    private final String fileYamlPath2 = pathToResources + "plain2.yaml";
+    private final String emptyJson = pathToResources + "emptyFile.json";
+
+    private final String expectedStylish = pathToResources + "resultStylish.txt";
 
     @Test
-    void compareJsonFormatTest() throws IOException {
-        String fileJsonPath1 = pathToResources + "plain1.json";
-        String fileJsonPath2 = pathToResources + "plain2.json";
-
-        String expected = Files.readString(Path.of(pathToResources + "resultJson.txt"));
+    void generate_Stylish_Diff_With_Relative_Path_Json() throws IOException {
+        String expected = Files.readString(Path.of(expectedStylish));
         String actual = Differ.generate(fileJsonPath1, fileJsonPath2, "stylish");
 
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void compareYamlFormatTest() throws IOException {
-        String fileYamlPath1 = pathToResources + "plain1.yaml";
-        String fileYamlPath2 = pathToResources + "plain2.yaml";
+    void generate_Stylish_Diff_With_Absolute_Path_Json() throws IOException {
+        String expected = Files.readString(Path.of(expectedStylish).toAbsolutePath());
+        String actual = Differ.generate(fileJsonPath1, fileJsonPath2, "stylish");
 
-        String expected = Files.readString(Path.of(pathToResources + "resultYaml.txt"));
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void generate_Stylish_Diff_With_Relative_Path_YAML() throws IOException {
+        String expected = Files.readString(Path.of(expectedStylish));
         String actual = Differ.generate(fileYamlPath1, fileYamlPath2, "stylish");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void generate_Stylish_Diff_With_Absolute_Path_YAML() throws IOException {
+        String expected = Files.readString(Path.of(expectedStylish).toAbsolutePath());
+        String actual = Differ.generate(fileYamlPath1, fileYamlPath2, "stylish");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void generate_Stylish_Diff_With_Relative_Path_JSON_YAML() throws IOException {
+        String expected = Files.readString(Path.of(expectedStylish));
+        String actual = Differ.generate(fileJsonPath1, fileYamlPath2, "stylish");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void generate_Stylish_Diff_With_Absolute_Path_JSON_YAML() throws IOException {
+        String expected = Files.readString(Path.of(expectedStylish).toAbsolutePath());
+        String actual = Differ.generate(fileJsonPath1, fileYamlPath2, "stylish");
 
         Assertions.assertEquals(expected, actual);
     }
